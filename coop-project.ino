5f8b18f649cedd62c4ed1e9f97e79929ecccc922
trickynick <Me@Nicholass-iMac-1105.local>
2016-04-30 17:06:05 -0700

    basic stjatemachine

--- coop-project.ino
+++ coop-project.ino
@@ -6,6 +6,8 @@ int closePin = 2;  //close relay
 int openPin = 4;   // open relay
 
 
+
+
 void setup()
 {
   pinMode(tempPin, INPUT);
@@ -18,37 +20,76 @@ void setup()
 
 unsigned long motionTime; //Start the clock
 
+// POSSIBLE STATES
+enum STATE_T {
+  STATE_OPEN,
+  STATE_WAITING,
+  STATE_CLOSED
+};
+
+
+int state = STATE_OPEN; // which state do we start in???
+int nextState; // don't initialize
+
+
 void loop()
 {
   
-
-  int reading = analogRead(tempPin);
-
-
-  float voltage = reading * 5.0;
-  voltage /= 1024.0;
-
-
-  Serial.print(voltage); Serial.println(" volts");
-
-  float temperatureC = (voltage - 0.5) * 100 ;
-
-  Serial.print(temperatureC); Serial.println(" degrees C");
-
-
-  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
-  Serial.print(temperatureF); Serial.println(" degrees F");
-  Serial.println(motionTime);
-  delay(1000);
-  
-  float lightValue = analogRead(lightPin);
-  Serial.print(lightValue); Serial.println(" units of photo-sensor");
-  
-  if (lightValue < 200)
+  nextState = state;
+  switch(state)
   {
-     // if PIR senses motion, save time
-     motionTime = millis();     
-     Serial.println("UPDARING");
-  }  
+    case STATE_OPEN:
+      Serial.println("OPEN");
+      break;
+    case STATE_WAITING:
+      Serial.println("WAITING");
+      break;
+    case STATE_CLOSED:
+      Serial.println("CLOSED");
+      break;
+    default:
+      Serial.println("SOMETHING WRONG");
+      break;
+  }
+  
+  state = nextState;
+  
+  delay(500);
+
+  
+  
+  
+  
+  
+  
+
+//  int reading = analogRead(tempPin);
+//
+//
+//  float voltage = reading * 5.0;
+//  voltage /= 1024.0;
+//
+//
+//  Serial.print(voltage); Serial.println(" volts");
+//
+//  float temperatureC = (voltage - 0.5) * 100 ;
+//
+//  Serial.print(temperatureC); Serial.println(" degrees C");
+//
+//
+//  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+//  Serial.print(temperatureF); Serial.println(" degrees F");
+//  Serial.println(motionTime);
+//  delay(1000);
+//  
+//  float lightValue = analogRead(lightPin);
+//  Serial.print(lightValue); Serial.println(" units of photo-sensor");
+//  
+//  if (lightValue < 200)
+//  {
+//     // if PIR senses motion, save time
+//     motionTime = millis();     
+//     Serial.println("UPDARING");
+//  }  
   
 }
